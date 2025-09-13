9f7cb95521f693ac049effe9d5b8965d5aa4ee73
JP <jota.tiger.dev@gmail.com>
2025-09-12 23:36:01 -0300

    create server api with fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
-console.log("hello world");
+import fastify from "fastify";
+
+const server = fastify({ logger: true  });
+
+server.get("/teams", async (request, response) => {
+    response.type("application/json").code(200);
+
+    return [
+        {
+            id: 1,
+            name: "ferrari"
+        }
+    ]
+});
+
+server.listen({port: 3333}, () => {
+    console.log("Server running on port 3333")
+})
+
+
